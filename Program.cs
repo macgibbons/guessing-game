5888b31810bd0cc6cc73972d3ba7bdde83c4a47b
macgibbons <macgibbons@gmail.com>
2020-02-27 15:01:50 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace GuessingGame {
+    class Program {
+        static void Main (string[] args) {
+            Random rand = new Random ();
+            int secretNumber = rand.Next (1, 11);
+        }
+    }
+}
