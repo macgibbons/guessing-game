505dab4a3e0f8ef8237d9010b7ecd9416766a46d
macgibbons <macgibbons@gmail.com>
2020-02-27 15:41:43 -0600

    game works

--- Program.cs
+++ Program.cs
@@ -5,6 +5,22 @@ namespace GuessingGame {
         static void Main (string[] args) {
             Random rand = new Random ();
             int secretNumber = rand.Next (1, 11);
+
+            for(int i =0; i < 3; i++){
+
+                Console.WriteLine ("Guess a number between 1 and 10!");
+                string userGuess = Console.ReadLine ();
+                int userGuessInt = int.Parse (userGuess);
+
+                if (userGuessInt == secretNumber) {
+                    Console.WriteLine ($"You guessed it! it was {secretNumber}");
+                    return;
+                } else {
+                    Console.WriteLine ("Nope!");
+
+                }
+            }
+
         }
     }
 }
